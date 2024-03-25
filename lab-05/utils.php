707d8be6ad9365e14f2c114ec6a5dadb38f916df
4-110-4 <4-110-4@nje.local>
2024-03-25 11:09:56 +0100

    asd

---
+++ lab-05/utils.php
@@ -0,0 +1,10 @@
+<?php
+    //Utility function for creating html select elements
+    function generateNumericSelect($name, $from, $to) {
+        echo "<select name=\"$name\" class=\"form-select\">";
+        for ($i=$from; $i <= $to; $i++) { 
+            echo"<option value=\"$i\">$i</option>";
+        }
+        echo "</select>";
+    }
+?>
